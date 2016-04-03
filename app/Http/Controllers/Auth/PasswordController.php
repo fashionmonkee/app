4d8f41909f35262477be03b0530b7c7ecad6d978
fadindia <fadindia2015@gmail.com>
2016-04-03 23:15:53 +0530

    .

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -2,8 +2,28 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Mail;
+use Validator;
+use Illuminate\Http\Request;
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\PasswordBroker;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Hash;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTFactory;
+use DB;
+use Auth;
+
+
+
+
 
 class PasswordController extends Controller
 {
@@ -20,13 +40,114 @@ class PasswordController extends Controller
 
     use ResetsPasswords;
 
+    
+
     /**
      * Create a new password controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Guard $auth, PasswordBroker $passwords)
     {
+        $this->auth = $auth;
+        $this->passwords = $passwords;
         $this->middleware('guest');
+    }
+
+    public function getEmail(){
+        return view('auth.password');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function postEmail(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email']);
+
+        $response = $this->passwords->sendResetLink($request->only('email'), function($m)
+        {
+            $m->subject($this->getEmailSubject());
+        });
+        
+        log::info("post email ------->");
+        log::info($response);
+
+        switch ($response)
+        {
+            case PasswordBroker::RESET_LINK_SENT:
+                return redirect()->back()->with('status', trans($response));
+
+            case PasswordBroker::INVALID_USER:
+                return redirect()->back()->withErrors(['email' => trans($response)]);
+        }
+    }
+
+    /**
+     * Get the e-mail subject line to be used for the reset link email.
+     *
+     * @return string
+     */
+    protected function getEmailSubject()
+    {
+        return isset($this->subject) ? $this->subject : 'Your Password Reset Link';
+    }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * @param  string  $token
+     * @return Response
+     */
+    public function getReset($token = null)
+    {
+        if (is_null($token))
+        {
+            throw new NotFoundHttpException;
+        }
+
+        return view('auth.reset')->with('token', $token);
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function postReset(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+
+        $credentials = $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+
+        $response = $this->passwords->reset($credentials, function($user, $password)
+        {
+            $user->password = bcrypt($password);
+
+            $user->save();
+
+            $this->auth->login($user);
+        });
+
+        switch ($response)
+        {
+            case PasswordBroker::PASSWORD_RESET:
+                return redirect($this->redirectPath());
+
+            default:
+                return redirect()->back()
+                            ->withInput($request->only('email'))
+                            ->withErrors(['email' => trans($response)]);
+        }
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,15 +8,21 @@ Route::get('shops/{id?}', 'ShopController@getShops');
 Route::get('reviews', 'ReviewController@getReviews');
 Route::get('ratings', 'RatingController@getRatings');
 Route::get('users/{id?}', 'UserController@getUsers');
+Route::get('reset','Auth\PasswordController@getEmail');
+Route::get('password/reset/{token?}','Auth\PasswordController@getReset');
+
+
 
 
 
 Route::post('login', 'Auth\AuthController@login');
 Route::post('register', 'Auth\AuthController@register');
+Route::post('password/email','Auth\PasswordController@postEmail');
 Route::post('users', 'UserController@updateProfile');
 Route::post('ratings', 'RatingController@postRatings');
 Route::post('reviews', 'ReviewController@postReviews');
 Route::post('shops', 'ShopController@postShops');
+Route::post('password/reset','Auth\PasswordController@postReset');
 
 
 Route::delete('reviews', 'ReviewController@deleteReviews');
