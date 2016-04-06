5ef39ec696e4b6af18dc876a88a4b5ae93b42250
naveen.s <naveen92>
2016-04-06 12:07:10 +0530

    landing page added

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -3,6 +3,15 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
 </head>
+ <nav role="navigation" class="navbar navbar-default">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">           
+            <a href="" class="navbar-brand"><strong>Fashion Monkee</strong></a>
+        </div>
+    </nav>
+</div>
+</body>
+</html>                                     
 <form method="POST" action="/password/reset">
     {!! csrf_field() !!}
     <input type="hidden" name="token" value="{{ $token }}">
@@ -36,10 +45,9 @@
         </div>
         <div class="row">
             <div class="col-sm-6 col-sm-offset-3">
-                <p class="text-center">Use the form below to change your password.</p>
-                 <input type="email" class="input-lg form-control" name="email" value="{{ old('email') }}" placeholder="Email">
-                <input type="password" class="input-lg form-control" name="password" id="password" placeholder="New Password" autocomplete="off">
-                <input type="password" class="input-lg form-control" name="password_confirmation" id="password_confirmation" placeholder="Repeat Password" autocomplete="off">
+                <input style="margin-bottom: 12px;" type="email" class="input-lg form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                <input style="margin-bottom: 12px;" type="password" class="input-lg form-control" name="password" id="password" placeholder="New Password" autocomplete="off">
+                <input style="margin-bottom: 12px;" type="password" class="input-lg form-control" name="password_confirmation" id="password_confirmation" placeholder="Repeat Password" autocomplete="off">
                 <input type="submit" class="col-xs-12 btn btn-primary btn-load btn-lg" data-loading-text="Changing Password..." value="Change Password">
             </div><!--/col-sm-6-->
         </div><!--/row-->
